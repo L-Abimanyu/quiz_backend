b7d9657e9c6a39496cfb6e07deac6765562f0ab9
Abimanyu <abim7562@gmail.com>
2024-07-31 13:56:38 +0530

    update render

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 const server = http.createServer(app);
 
 const corsOptions = {
-  origin: "https://spiffy-unicorn-e8b85a.netlify.app",
+  origin: "https://quiz-frontend-2qv5.onrender.com",
   methods: ["GET", "POST"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
